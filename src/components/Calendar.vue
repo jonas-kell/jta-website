c68e6fc49e36af1f7fdd9b2c069e9d4dcf12c549
Jonas Kell <jonas-kell@web.de>
2025-02-01 19:22:15 +0100

    feat: embed test and calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,7 +1,16 @@
 <template>
-    Calendar
+    <h3>
+        <router-link to="/">Zurück zur Zeitnahme Übersicht</router-link>
+    </h3>
 
-    <router-link to="/">Back</router-link>
+    <iframe
+        src="https://calendar.google.com/calendar/embed?src=32f93d13eecfc4cdd9f185dc81d5104dc639c366df70200b8c81d71e69abe123%40group.calendar.google.com&ctz=Europe%2FBerlin&showPrint=0&title=Zeitnahme%20Anlage%20Schwaben&showCalendars=0&showTz=0&mode=AGENDA&color=%23D50000"
+        style="border: 0"
+        width="800"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+    ></iframe>
 </template>
 
 <script setup lang="ts">
