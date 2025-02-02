4309ae268bb7109babdb7409090aaade7b94e2cf
Jonas Kell <jonas-kell@web.de>
2025-02-02 18:18:01 +0100

    feat: finalized initial design

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,6 +4,34 @@
     </h3>
 
     <h2>Kontakt</h2>
+
+    <p>Anfragen für die Miete und oder Reservierung der Anlage an:</p>
+
+    <p>
+        <a href="mailto:zeitnahme@blv-schwaben.de">zeitnahme@blv-schwaben.de</a>
+    </p>
+
+    <h3>Verantwortlicher Zeitmessung:</h3>
+    <p>Jonas Kell (TSV Schwabmünchen)</p>
+
+    <h3>Zubuchung eines Anlagenbetreuers und/oder Zeitmessers:</h3>
+    <img src="/public/tsv-smue-experts.jpg" class="small-image" />
+    <p class="multiline-p">
+        Grundsätzlich ist die Miete der Anlage ohne Lieferung, Auf-/Abbau oder Betrieb. <br />
+        Die buchenden Vereine sind selbst hierfür verantwortlich. <br />
+        Hierzu muss vorher die entsprechende Fortbildung/Schulung erfolg sein. <br />
+        Der TSV-Schwabmünchen hat ausgebildete Verantwortliche, die bei Bedarf engagiert werden können. <br />
+        Dies geschieht unabhängig von der Anlagenmiete und unabhängig <br />
+        von dem an den Bezirk Schwaben zu entrichtenden Mietbetrag.<br />
+        Auch Transport/Zeitmess-Wohnmobil sind unabhängig von der Miete extra zu beantragen.<br />
+        Bei Bedarf unbedingt bei Kontaktaufnahme ansprechen!!
+    </p>
+
+    <p>
+        <a href="mailto:zeitnahme@blv-schwaben.de">zeitnahme@blv-schwaben.de</a>
+    </p>
+
+    <p>!! Beachte Transportanforderungen für Videowall, siehe <router-link to="download">Downloads</router-link> !!</p>
 </template>
 
 <script setup lang="ts">
--- src/components/Price.vue
+++ src/components/Price.vue
@@ -4,6 +4,22 @@
     </h3>
 
     <h2>Preis / Angebot</h2>
+
+    <p>Die Zeitmessanlage kann in mehreren Konfigurationen/Teilen gebucht werden.</p>
+    <p>Erklärung zusätzliche Leistungen siehe auch <router-link to="contact">Kontakt</router-link>.</p>
+
+    <h3>Anlagemiete gegenüber dem Bezirk Schwaben:</h3>
+
+    <p>Miete Zeitmessung pro Tag: &nbsp;&nbsp;&nbsp;&nbsp; 250€</p>
+    <p>Miete Anzeigetafel pro Tag: &nbsp;&nbsp;&nbsp;&nbsp; 50€</p>
+    <p>Miete Geräte Messset pro Tag: &nbsp;&nbsp;&nbsp;&nbsp; 10€</p>
+
+    <h3>Externes Engagieren von Zeitnehmern gegenüber dem TSV-Schwabmünchen:</h3>
+
+    <p>Engagieren Zeitnehmer pro Stunde: &nbsp;&nbsp;&nbsp;&nbsp; 20€ oder im Paket auf Anfrage</p>
+    <p>Anlagentransport/Abholung: &nbsp;&nbsp;&nbsp;&nbsp; auf Anfrage</p>
+    <p>Zeitnahme-Wohnmobil: &nbsp;&nbsp;&nbsp;&nbsp; auf Anfrage</p>
+    <p>Auf-/Abbauunterstützung und Problemlöse-Helfer bei Eigenbetreuung: &nbsp;&nbsp;&nbsp;&nbsp; auf Anfrage</p>
 </template>
 
 <script setup lang="ts">
