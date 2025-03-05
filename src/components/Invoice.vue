980cfc3f2d6aeab88eaf813694567bc102c1b7a4
Jonas Kell <jonas-kell@web.de>
2025-03-05 12:47:42 +0100

    feat: start data entry portion

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -1,7 +1,15 @@
 <template>
     <h2>Rechnungs-Generator</h2>
 
-    <button @click="test">test</button>
+    <div class="form-body">
+        <p>Eigener Name: <input type="text" v-model="ownName" /></p>
+        <p>Eigene Straße: <input type="text" v-model="ownStreet" /></p>
+        <p>Eigene Stadt: <input type="text" v-model="ownCity" /></p>
+        <p>Eigene Tel.: <input type="text" v-model="ownTel" /></p>
+        <p>Eigene Mail: <input type="text" v-model="ownMail" /></p>
+    </div>
+
+    <button @click="generatePDF">Rechnung Generieren</button>
 </template>
 
 <script setup lang="ts">
@@ -10,10 +18,18 @@
     appStore.dark; // just that the app store works and doesn't give unused errors
     import { jsPDF } from "jspdf";
     import imageData from "./../public/Logo-Schwaben.png?base64";
+    import { ref } from "vue";
 
-    function test() {
-        console.log("test");
+    const OBFUSCATION_KEY = 42; // Any number (should be consistent)
+    // the addresses and semi-personal default-data are obfuscated in the source to not get it web-scraped (easily)
 
+    const ownName = ref(deobfuscate("YEVES1kKYU9GRg=="));
+    const ownStreet = ref(deobfuscate("eUlCS19dQ09ZWV5YBAobGA=="));
+    const ownCity = ref(deobfuscate("EhwSGRoKeUlCXUtIR9ZESUJPRA=="));
+    const ownTel = ref(deobfuscate("AR4TChsfHRkKGBwbHxsYGg=="));
+    const ownMail = ref(deobfuscate("UE9DXkRLQkdPakhGXAdZSUJdS0hPRAROTw=="));
+
+    function generatePDF() {
         const doc = new jsPDF();
 
         doc.text("Hello world!", 10, 10);
@@ -32,6 +48,37 @@
 
         doc.save("a4.pdf");
     }
+
+    function obfuscate(text: string) {
+        if (text == "") {
+            return "";
+        }
+
+        return btoa([...text].map((c) => String.fromCharCode(c.charCodeAt(0) ^ OBFUSCATION_KEY)).join(""));
+    }
+
+    function deobfuscate(encoded: string) {
+        if (encoded == "") {
+            return "";
+        }
+
+        return [...atob(encoded)].map((c) => String.fromCharCode(c.charCodeAt(0) ^ OBFUSCATION_KEY)).join("");
+    }
+
+    // Example Usage for the obfuscation (do not commit the "secret" things obviously)
+    const original = "testtesttest";
+    const obfuscated = obfuscate(original);
+    console.log(obfuscated);
 </script>
 
-<style scoped></style>
+<style scoped>
+    .form-body {
+        text-align: left;
+        width: 90vw;
+        min-width: 500px;
+        margin-left: 2em;
+        margin-right: 2em;
+        padding-left: 2em;
+        padding-right: 2em;
+    }
+</style>
